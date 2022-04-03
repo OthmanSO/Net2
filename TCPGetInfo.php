5e40a32a7e37443b0f63b96965e7300f2873b5e2
amreshtiwi <ammr64@outlook.com>
2022-04-03 10:16:56 +0300

    edit the the TCPGetInfo.php to work correct and edit the tcp.html script to split the data

--- TCPGetInfo.php
+++ TCPGetInfo.php
@@ -4,57 +4,20 @@ function getTCPInfo(){
     $ip = "127.0.0.1:161";
 
 
-    $ipAddr = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.20.1.1");
-    $ipIf = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.20.1.2");
-    $ipNetMask = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.20.1.3");
-    $ipAdEntBcastAddr = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.20.1.4");
-    $ipMaxSize = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.20.1.5");
+    $tcpConnLocalAddress = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.6.13.1.2");
+    $tcpConnLocalPort = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.6.13.1.3");
+    $tcpConnRemAddress = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.6.13.1.4");
+    $tcpConnRemPort	 = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.6.13.1.5");
 
-    $ipNetToMediaIfIndex = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.22.1.1");
-    $ipNetToMediaPhysAddress = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.22.1.2");
-    $ipNetToMediaNetAddress = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.22.1.3");
-    $ipNetToMediaType = snmp2_walk($ip, "snmp", ".1.3.6.1.2.1.4.22.1.4");
+    $arrLen = count($tcpConnLocalAddress)-1 ;
+    $returnText ="";
+    for($i = 0; $i < $arrLen ; $i++ )
+        $returnText= $returnText . substr($tcpConnLocalAddress[$i],10) . "," . substr($tcpConnLocalPort[$i],9) . "," 
+        . substr($tcpConnRemAddress[$i],10) . "," . substr($tcpConnRemPort[$i],9) . "|&&&|";
 
+    $returnText = $returnText . substr($tcpConnLocalAddress[$arrLen],10) . "," . substr($tcpConnLocalPort[$arrLen],9) . "," 
+    . substr($tcpConnRemAddress[$arrLen],10) . "," . substr($tcpConnRemPort[$arrLen],9) ;
 
-    return ipAddressString( $ipAddr , $ipIf , $ipNetMask , $ipAdEntBcastAddr , $ipMaxSize ) . "|&&&|" 
-    . ipNetToMediaTableString( $ipNetToMediaIfIndex , $ipNetToMediaPhysAddress , $ipNetToMediaNetAddress , $ipNetToMediaType );
-}
+    return $returnText;
 
-function ipAddressString( $ipAddr , $ipIf , $ipNetMask , $ipAdEntBcastAddr , $ipMaxSize ){
-    $ipAddrStr = "";
-    $arrLen = count($ipAddr) ;
-    for($i = 0; $i < $arrLen - 1 ;$i++ )  
-        $ipAddrStr = $ipAddrStr . substr($ipAddr[$i],11) . "|&|"
-                                . substr($ipIf[$i],9) . "|&|"
-                                . substr($ipNetMask[$i],11) . "|&|"
-                                . substr($ipAdEntBcastAddr[$i],9) . "|&|"
-                                . substr($ipMaxSize[$i],9) . "|&&|";
-        
-    $ipAddrStr = $ipAddrStr . substr($ipAddr[$arrLen-1],11) . "|&|"
-                            . substr($ipIf[$arrLen-1],9) . "|&|"
-                            . substr($ipNetMask[$arrLen-1],11) . "|&|"
-                            . substr($ipAdEntBcastAddr[$arrLen-1],9) . "|&|"
-                            . substr($ipMaxSize[$arrLen-1],9);
-    return $ipAddrStr;
-}
-
-
-
-function ipNetToMediaTableString( $ipNetToMediaIfIndex , $ipNetToMediaPhysAddress , $ipNetToMediaNetAddress , $ipNetToMediaType ){
-    $ipToMedia ="";
-    $lengthOfTheArr = count($ipNetToMediaIfIndex);
-    for($i = 0; $i < $lengthOfTheArr - 1;$i++ )  
-        $ipToMedia = $ipToMedia . substr($ipNetToMediaIfIndex[$i],9) . "|&|"
-                                . substr($ipNetToMediaIfIndex[$i],9) . "|&|"
-                                . substr($ipNetToMediaPhysAddress[$i],11) . "|&|"
-                                . substr($ipNetToMediaNetAddress[$i],11) . "|&|"
-                                . substr($ipNetToMediaType[$i],9) . "|&&|" ;
-
-    $ipToMedia = $ipToMedia . substr($ipNetToMediaIfIndex[$lengthOfTheArr-1],9) . "|&|"
-                            . substr($ipNetToMediaIfIndex[$lengthOfTheArr-1],9) . "|&|"
-                            . substr($ipNetToMediaPhysAddress[$lengthOfTheArr-1],11) . "|&|"
-                            . substr($ipNetToMediaNetAddress[$lengthOfTheArr-1],11) . "|&|"
-                            . substr($ipNetToMediaType[$lengthOfTheArr-1],9) ;
-
-    return $ipToMedia ;            
 }
